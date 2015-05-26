6191bd5d634cca6af892f43a5f15818506f71a3c
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-05-26 15:46:02 -0400

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
 	has_many :user_subscriptions
-	has_many :subscriptions, through :user_subscriptions
+	has_many :subscriptions, through: :user_subscriptions
 end
