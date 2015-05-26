327c5ef551a4fd0c5a0754af4542d11ac58ee6ed
Peter Piné <peterpine83@Peter-Pines-MacBook-Air.local>
2015-05-26 14:38:11 -0400

    set up many to many

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,5 @@
 class Subscription < ActiveRecord::Base
+	has_many :user_subscriptions
+	had_many :users, through :user_subscriptions
+
 end
--- app/models/user_subscription.rb
+++ app/models/user_subscription.rb
@@ -1,2 +1,4 @@
 class UserSubscription < ActiveRecord::Base
+	belongs_to :user 
+	belongs_to :subscription
 end
