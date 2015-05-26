dc1bcbfb913a67b924d0943fe5c6833cb855920d
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-05-26 15:46:02 -0400

    seeds

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ActiveRecord::Base
 	has_many :user_subscriptions
-	had_many :users, through :user_subscriptions
+	has_many :users, through: :user_subscriptions
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'bruce', password: 'password')
+User.create(name: 'alfred', password: 'password')
+
+Subscription.create(name: "twitter", keyword: "taylor swift", url: "www.testUrl.org/")
+Subscription.create(name: "twitter", keyword: "batman", url: "www.testUrl.org/")
+Subscription.create(name: "twitter", keyword: "joker", url: "www.testUrl.org/")
+Subscription.create(name: "twitter", keyword: "celine dion", url: "www.testUrl.org/")
+Subscription.create(name: "twitter", keyword: "winston churchill", url: "www.testUrl.org/")
+
+UserSubscription.create(user_id: 0, subscription_id: 2)
+UserSubscription.create(user_id: 0, subscription_id: 1)
+UserSubscription.create(user_id: 0, subscription_id: 0)
+UserSubscription.create(user_id: 1, subscription_id: 4)
+UserSubscription.create(user_id: 1, subscription_id: 3)
+UserSubscription.create(user_id: 1, subscription_id: 0)
+UserSubscription.create(user_id: 1, subscription_id: 2)
