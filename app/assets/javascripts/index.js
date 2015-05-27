a4d8f36511ada86e4588622177071912db42bdfc
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-05-27 13:09:09 -0400

    obtain subscription list for one user

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     xhr.open('GET', 'http://localhost:3000/subscriptions.json');
     xhr.addEventListener('load', function() {
       var subs = JSON.parse(xhr.responseText);
+      
+      checkedStatus = isChecked();
+      console.log(checkedStatus);
+
       subs.forEach(function(sub) {
         addSub(sub); //go to line 30
       })
@@ -27,7 +31,21 @@ document.addEventListener('DOMContentLoaded', function() {
     xhr.send();
   };
 
+   var isChecked = function(){
+      id = 1; // eventually this id will be defined by the login authentication
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'http://localhost:3000/user_subscriptions/'+ id);
+      xhr.addEventListener('load', function() {
+        
+          var response = JSON.parse(xhr.responseText);
+          console.log(response);
+        
+      });
+      xhr.send();
+  }
+
   var addSub = function(sub) {
+    
     var li = document.createElement('li');
     setLiToSub(li, sub); // go to line 37
     var ul = document.getElementById('subsList')
@@ -38,9 +56,13 @@ document.addEventListener('DOMContentLoaded', function() {
     li.setAttribute('id', 'sub' + sub.id);
     li.innerHTML = "";
 
+    var subCheck = document.createElement('input');
+    subCheck.type = "checkbox";
     var subText = sub.keyword;
     var subTextNode = document.createTextNode(subText);
+    li.appendChild(subCheck);
     li.appendChild(subTextNode);
+    
 
     // var edit = document.createElement('button');
     // edit.innerHTML = "Edit";
@@ -54,6 +76,9 @@ document.addEventListener('DOMContentLoaded', function() {
     deleteButton.addEventListener('click', deleteSub); //go to line 16
     li.appendChild(deleteButton);
   }
+
+  
+
 
   // var editSub = function(li, name, type) {
   //   li.innerHTML = '';
