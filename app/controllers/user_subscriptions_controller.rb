fd793d971f112e3f4063ba7ce891f71e95fe7418
Steve McIntosh <scm.phub@gmail.com>
2015-05-27 10:22:12 -0400

    controllers are coming together: render json

--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -2,4 +2,29 @@ class UserSubscriptionsController < ApplicationController
 	protect_from_forgery with: :null_session
 
 	def index
-		@user_subscriptions = User_subsciption.all
+		@user_subscriptions = UserSubscription.all
+		respond_to do |format|
+			format.html { render :index }
+			format.json { render json: @user_subscriptions }
+		end
+	end
+
+	def show
+		@user_subscription = UserSubscription.find_by(id: params[:id])
+		if @user_subsciption
+			render json: @user_subscription
+		else
+			render status: 400, nothing: true
+		end
+	end
+
+	# create
+	# update
+	# destroy
+
+	private
+	def user_subscription_params
+		params.require(:user_subscription).permit(:user_id, :subscription_id)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   "subscriptions#index"
   resources :subscriptions
   resources :users
+  resources :user_subscriptions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
