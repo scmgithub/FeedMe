29964f7a958644bead52b8d558953853dfb01812
Dipshikha Biswas <Dipshikha@Dipshikhas-MacBook-Pro.local>
2015-05-28 14:21:05 -0400

    authen-2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(name: params[:user_name])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to "/secret"
+			redirect_to "/subscriptions"
 		else
 			@error = true
 			render :new
