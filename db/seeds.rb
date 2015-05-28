55979ff1c5745e7161dbc9f90c5d0b3577760dc2
Steve McIntosh <scm.phub@gmail.com>
2015-05-28 16:01:25 -0400

    content showing up on front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,19 @@
 User.create(name: 'bruce', password: 'password')
 User.create(name: 'alfred', password: 'password')
 
-Subscription.create(name: "twitter", keyword: "taylor swift", url: "www.t-swizz.org/")
-Subscription.create(name: "twitter", keyword: "batman", url: "www.batman.org/")
-Subscription.create(name: "twitter", keyword: "joker", url: "www.joker.org/")
-Subscription.create(name: "twitter", keyword: "celine dion", url: "www.canadarox.org/")
-Subscription.create(name: "twitter", keyword: "winston churchill", url: "www.winston.org/")
+# Subscription.create(name: "twitter", keyword: "taylor swift", url: "www.t-swizz.org/")
+# Subscription.create(name: "twitter", keyword: "batman", url: "www.batman.org/")
+# Subscription.create(name: "twitter", keyword: "joker", url: "www.joker.org/")
+# Subscription.create(name: "twitter", keyword: "celine dion", url: "www.canadarox.org/")
+# Subscription.create(name: "twitter", keyword: "winston churchill", url: "www.winston.org/")
 #Subscription.create(name: "NYTimes", keyword: "pugs", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=pugs&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+
+Subscription.create(name: "twitter", keyword: "taylor swift", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=taylor+swift&fl=web_url,snippet,source,headline,pub_date&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+Subscription.create(name: "twitter", keyword: "batman", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=batman&fl=web_url,snippet,source,headline,pub_date&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+Subscription.create(name: "twitter", keyword: "joker", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=joker&fl=web_url,snippet,source,headline,pub_date&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+Subscription.create(name: "twitter", keyword: "celine dion", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=celine+dion&fl=web_url,snippet,source,headline,pub_date&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+Subscription.create(name: "twitter", keyword: "winston churchill", url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=winston+churchill&fl=web_url,snippet,source,headline,pub_date&api-key=aa1260c72c4d669a7ecd0d1aa8725430:6:8505067")
+
 
 UserSubscription.create(user_id: 1, subscription_id: 3)
 UserSubscription.create(user_id: 1, subscription_id: 2)
